e469fa552b71b16ea71f91677d74ddc3566882f6
Franklin Velasquez <iefranklin@hotmail.es>
2023-04-02 16:36:47 -0600

    add Form view and logic, sections, data binding

--- Movie Tracker/ContentView.swift
+++ Movie Tracker/ContentView.swift
@@ -11,71 +11,83 @@ import CoreData
 // it is a "struct" not a "class" so is doesn't inherit something, just meet some protocol
 struct ContentView: View {
     
+    @State var title = ""
+    @State var rating = 3.0
+    @State var seeing = false
+    
     var body: some View {
         
-        // The order of the modifiers matters !
-        
-        // Text("Hello World 1").padding().background(Color.blue)
-        // Text("Hello World 2").background(Color.blue).padding()
-        // Text("Hello World 3").background(Color.blue).padding().background(Color.green)
-        
-        // Working with Images
-        // Image(systemName: "car")
-        //    .resizable()
-        //    .edgesIgnoringSafeArea([.all])
-        //    .aspectRatio(contentMode: .fit).foregroundColor(.orange)
-        
-        VStack(alignment: .center) {
-            Text("Hello really big text: opqwjeioaslkamvlxvkliouw")
-                .lineLimit(1).font(.largeTitle)
-           //     .layoutPriority(1) // displays completely this item and reduce the others
+        List {
             
-            Image(systemName: "lightbulb")
-            
-            
-            Text("Goodby")
-                .lineLimit(1)
-            
-            
-            ZStack{
-                Image("code").resizable().aspectRatio(contentMode: .fit)
+            Section{
+                SectionTitle(title: "Title")
                 
-                Button(action: hello) {
-                    Image(systemName: "play.fill").resizable().foregroundColor(.red)
-                        .aspectRatio(contentMode: .fit).frame(width: 100,height: 100)
-                }
-                
+                TextField("Movie Title",text: $title)
             }
             
-            Button {
+            // a section let's group things like a stack
+            Section {
                 
-            } label: {
-                VStack{
-                    Image(systemName: "gamecontroller")
-                    Text("Tap Me")
+                SectionTitle(title: "Rating")
+                
+                HStack{
+                    Spacer()
+                    Text(String(repeating: "⭐️", count: Int(rating)))
+                        .font(.title)
+                    Spacer()
                 }
                 
-            }.padding(.all,20)
-                .background(Color.green)
-                .cornerRadius(10)
-                .foregroundColor(.white)
-                
-
+                Slider(value: $rating, in: 1...5 , step: 1)
+            }
             
+            Section{
+                
+                SectionTitle(title: "Seen")
+                
+                Toggle(isOn: $seeing){
+                    
+                    if title == ""{
+                        Text("I have seen this movie")
+                    } else {
+                        Text("I have seen \(title)")
+                    }
+                }
+            }
+            
+            Section{
+                Button {
+                    
+                } label: {
+                    HStack{
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    }
+                }
 
-        }
-        
+            }
+            
+            
+        }.listStyle(GroupedListStyle())
         
         
     }
     
-    func hello() {
-        print("Hello")
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+struct SectionTitle: View {
+    
+    var title = ""
+    
+    var body: some View {
+        Text(title)
+            .font(.caption)
+            .foregroundColor(.gray)
     }
 }
