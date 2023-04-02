1f68824ac68b89a0f7eb134377afd046726210fd
Franklin Velasquez <iefranklin@hotmail.es>
2023-04-02 16:17:24 -0600

    add Views, Stack, Images, Buttons simple exercises

--- Movie Tracker/ContentView.swift
+++ Movie Tracker/ContentView.swift
@@ -17,9 +17,60 @@ struct ContentView: View {
         
         // Text("Hello World 1").padding().background(Color.blue)
         // Text("Hello World 2").background(Color.blue).padding()
-        Text("Hello World 3").background(Color.blue).padding().background(Color.green)
+        // Text("Hello World 3").background(Color.blue).padding().background(Color.green)
+        
+        // Working with Images
+        // Image(systemName: "car")
+        //    .resizable()
+        //    .edgesIgnoringSafeArea([.all])
+        //    .aspectRatio(contentMode: .fit).foregroundColor(.orange)
+        
+        VStack(alignment: .center) {
+            Text("Hello really big text: opqwjeioaslkamvlxvkliouw")
+                .lineLimit(1).font(.largeTitle)
+           //     .layoutPriority(1) // displays completely this item and reduce the others
+            
+            Image(systemName: "lightbulb")
+            
+            
+            Text("Goodby")
+                .lineLimit(1)
+            
+            
+            ZStack{
+                Image("code").resizable().aspectRatio(contentMode: .fit)
+                
+                Button(action: hello) {
+                    Image(systemName: "play.fill").resizable().foregroundColor(.red)
+                        .aspectRatio(contentMode: .fit).frame(width: 100,height: 100)
+                }
+                
+            }
+            
+            Button {
+                
+            } label: {
+                VStack{
+                    Image(systemName: "gamecontroller")
+                    Text("Tap Me")
+                }
+                
+            }.padding(.all,20)
+                .background(Color.green)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+                
+
+            
+
+        }
         
         
+        
+    }
+    
+    func hello() {
+        print("Hello")
     }
 }
 
